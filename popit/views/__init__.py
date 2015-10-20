c73af45380836a5281738b932ee072132b30e613
sweemeng <sweester@gmail.com>
2015-10-21 06:15:20 +0800

    refactor views into directory, commit stub of organization views

---
+++ popit/views/__init__.py
@@ -0,0 +1,17 @@
+__author__ = 'sweemeng'
+from popit.views.persons import PersonDetail
+from popit.views.persons import PersonList
+from popit.views.misc import PersonContactDetail
+from popit.views.misc import PersonContactList
+from popit.views.misc import PersonContactLinkDetail
+from popit.views.misc import PersonContactLinkList
+from popit.views.misc import PersonIdentifierDetail
+from popit.views.misc import PersonIdentifierList
+from popit.views.misc import PersonIdentifierLinkDetail
+from popit.views.misc import PersonIdentifierLinkList
+from popit.views.misc import PersonOtherNameDetail
+from popit.views.misc import PersonOtherNameList
+from popit.views.misc import PersonOtherNameLinkDetail
+from popit.views.misc import PersonOtherNameLinkList
+from popit.views.misc import PersonLinkDetail
+from popit.views.misc import PersonLinkList
