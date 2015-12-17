a0a3dedcb068f2fccd16ff5a490d1d2e0a3a27e3
sweemeng <sweester@gmail.com>
2015-12-17 14:27:11 +0800

    And 100 item on one page is hard to read

--- popit_ng/settings.py
+++ popit_ng/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 10
 }
 
 # We might change host in production, but in dev and
