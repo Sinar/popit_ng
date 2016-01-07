9ab2fa72961861972e5c4a0f5373bfaa1dd1dff1
sweemeng <sweester@gmail.com>
2016-01-07 10:18:34 +0800

    implement issue #72
    * page must always return int

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ class PopitPaginator(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
-            ('page', self.page_number),
+            ('page', int(self.page_number)),
             ('total', self.page.paginator.count),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
