a3144a1bc233afde13c543e746e7bea9b23db706
sweemeng <sweester@gmail.com>
2016-04-21 11:26:24 +0800

    just update to install es from their deb repository #151

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,9 @@ sudo -u postgres psql -c "grant all privileges on database popit to popit"
 # DO NOT USE THE LINE BELOW IN PRODUCTION. THIS IS FOR VAGRANT ONLY
 sudo -u postgres psql -c "ALTER USER popit WITH CREATEDB;"
 apt-get install -y openjdk-7-jre unzip
-wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.1.0/elasticsearch-2.1.0.zip
-unzip elasticsearch-2.1.0.zip
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
 
 # Ensure when login via shell it goes to the code
 echo -e "\ncd /vagrant" >>~/.bashrc
