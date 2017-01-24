0072c77ce342885269bbc7e396809ea36a6bef0d
sweemeng <sweester@gmail.com>
2017-01-24 13:30:48 +0800

    Stupid mission slash in URL #240

--- popit_search/urls/advancesearch.py
+++ popit_search/urls/advancesearch.py
@@ -3,6 +3,6 @@ from popit_search.views import AdvanceSearchView
 
 
 urlpatterns = [
-        url(r"^(?P<entity>\w+)$", AdvanceSearchView.as_view(), name="advance_search"),
+        url(r"^/?(?P<entity>\w+)/?$", AdvanceSearchView.as_view(), name="advance_search"),
 
         ]
